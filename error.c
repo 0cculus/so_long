babb3f9e56b5f9f1f91f977046ecd6b2bb3489b8
Brian Rheaume-Michaud <brheaume@c2r5p9.42quebec.com>
2023-04-25 13:37:18 -0400

    added ft_error_interrupt, updated ft_error_game, removed char * parameter

--- error.c
+++ error.c
@@ -6,28 +6,37 @@
 /*   By: brheaume <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 16:26:20 by brheaume          #+#    #+#             */
-/*   Updated: 2023/04/14 14:42:41 by brheaume         ###   ########.fr       */
+/*   Updated: 2023/04/25 12:32:21 by brheaume         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	ft_error_path(char *message)
+void	ft_error_path(void)
 {
-	ft_putendl_fd(message, 0);
-	return (EXIT_FAILURE);
+	ft_putendl_fd("Invalid map path\n", ERROR_OUTPUT);
+	exit(EXIT_FAILURE);
 }
 
-int	ft_error_map(char **map, char *message)
+void	ft_error_map(char **map)
 {
-	ft_cleararray(map);
-	ft_putendl_fd(message, 0);
-	return (EXIT_FAILURE);
+	ft_clear_array(map);
+	ft_putendl_fd("Invalid map\n", ERROR_OUTPUT);
+	exit(EXIT_FAILURE);
 }
 
-// WIP
-int ft_error_game(t_game *game, char *message)
+void	ft_error_game(t_data *game)
 {
-	ft_cleararray(map);
-	mlx_terminate();
+	ft_clear_array(game->map);
+	mlx_terminate(game->mlx);
+	ft_putendl_fd("Game did an oopsie daisy", ERROR_OUTPUT);
+	exit(EXIT_FAILURE);
+}
+
+void	ft_error_interrupt(t_data *game)
+{
+	ft_clear_array(game->map);
+	mlx_terminate(game->mlx);
+	ft_putendl_fd("Interrupted!", ERROR_OUTPUT);
+	exit(EXIT_FAILURE);
 }
