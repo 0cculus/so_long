3ece24deee74d4a0e74fe74788ff08d2daeb390e
Brian Rheaume-Michaud <brheaume@c2r5p9.42quebec.com>
2023-04-25 13:35:46 -0400

    renamed ft_draw & ft_draw_terrain, added ft_draw_map & ft_redraw_collect

--- draw.c
+++ draw.c
@@ -6,35 +6,87 @@
 /*   By: brheaume <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 13:18:21 by brheaume          #+#    #+#             */
-/*   Updated: 2023/04/14 15:00:21 by brheaume         ###   ########.fr       */
+/*   Updated: 2023/04/25 10:39:28 by brheaume         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	ft_draw(t_game game, int x, int y, mlx_image_t *img)
+static void	ft_draw_nonconcurrent(t_data *game, int x, int y, mlx_image_t *img)
 {
-	if (game.map[y][x] == WALL_VAL)
-		ft_draw_terrain();
-	else if (game.map[y][x] == FLOOR_VAL)
-		ft_draw_terrain(game, x, y, img);
-	else if (game.map[y][x] == HERO_VAL)
-		ft_draw_superimposed(game, x, y, img);
-	else if (game.map[y][x] == COLLECT_VAL)
-		ft_draw_superimposed(game, x, y, img);
-	else if (game.map[y][x] == EXIT_CLOSED_VAL)
-		ft_draw_superimposed(game, x, y, img);
-	else if (game.map[y][x] == EXIT_OPEN_VAL)
-		ft_draw_superimposed(game, x, y, img);
+	mlx_image_to_window(game->mlx, img, x * SPRITE_SIZE, y * SPRITE_SIZE);
 }
 
-void	ft_draw_terrain(mlx_t mlx, int x, int y, mlx_image_t *img)
+void	ft_draw_superimposed(t_data *game, int x, int y, mlx_image_t *img)
 {
-	mlx_image_to_window(mlx, img, x * SPRITE_SIZE, y * SPRITE_SIZE);
+	ft_draw_nonconcurrent(game, x, y, game->images.floor);
+	mlx_image_to_window(game->mlx, img, x * SPRITE_SIZE, y * SPRITE_SIZE);
 }
 
-void	ft_draw_superimposed(t_game game, int x, int y, mlx_image_t *img)
+static void	ft_select_draw(t_data *game, int x, int y)
 {
-	ft_draw_terrain(&game.mlx, x, y, &game.im);
-	mlx_image_to_window(&game.mlx, &img, x * SPRITE_SIZE, y * SPRITE_SIZE);
+	if (game->map[y][x] == WALL_VAL)
+		ft_draw_nonconcurrent(game, x, y, game->images.wall);
+	else if (game->map[y][x] == FLOOR_VAL)
+		ft_draw_nonconcurrent(game, x, y, game->images.floor);
+	else if (game->map[y][x] == HERO_VAL)
+		ft_draw_nonconcurrent(game, x, y, game->images.floor);
+	else if (game->map[y][x] == COLLECT_VAL)
+		ft_draw_superimposed(game, x, y, game->images.collect);
+	else if (game->map[y][x] == EXIT_CLOSED_VAL)
+	{
+		game->x_exit = x;
+		game->y_exit = y;
+		ft_draw_superimposed(game, x, y, game->images.exit_close);
+	}
 }
+
+void	ft_map_draw(t_data *game)
+{
+	int	x;
+	int	y;
+	int	x_hero;
+	int	y_hero;
+
+	y = 0;
+	while (game->map[y])
+	{
+		x = 0;
+		while (game->map[y][x])
+		{
+			if (game->map[y][x] == HERO_VAL)
+			{
+				x_hero = x;
+				y_hero = y;
+				ft_draw_nonconcurrent(game, x, y, game->images.floor);
+			}
+			else
+				ft_select_draw(game, x, y);
+			x++;
+		}
+		y++;
+	}
+	ft_draw_nonconcurrent(game, x_hero, y_hero, game->images.hero);
+}
+
+void	ft_redraw_collect(t_data *game)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	mlx_delete_image(game->mlx, game->images.collect);
+	game->images.collect = ft_texture_load(game->mlx, COLLECT);
+	while (game->map[y])
+	{
+		x = 0;
+		while (game->map[y][x])
+		{
+			if (game->map[y][x] == COLLECT_VAL)
+				ft_draw_nonconcurrent(game, x, y, game->images.collect);
+			x++;
+		}
+		y++;
+	}
+}
+
