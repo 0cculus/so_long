1045ad92d1b6bfa9cb879abe22d5f54d8179c10d
Brian Rheaume-Michaud <brheaume@c2r5p9.42quebec.com>
2023-04-25 13:38:34 -0400

    added file verification if not exists

--- load.c
+++ load.c
@@ -6,18 +6,19 @@
 /*   By: brheaume <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 11:03:24 by brheaume          #+#    #+#             */
-/*   Updated: 2023/04/14 12:49:39 by brheaume         ###   ########.fr       */
+/*   Updated: 2023/04/25 13:18:26 by brheaume         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-mlx_image_t	ft_texture_load(mlx_t mlx, char *path)
+mlx_image_t	*ft_texture_load(mlx_t *mlx, char *path)
 {
 	mlx_texture_t	*texture;
-	mlx_image_t		image;
-	
+	mlx_image_t		*image;
+
 	texture = mlx_load_png(path);
+	image = mlx_new_image(mlx, SPRITE_SIZE, SPRITE_SIZE);
 	image = mlx_texture_to_image(mlx, texture);
 	mlx_delete_texture(texture);
 	return (image);
@@ -29,17 +30,22 @@ char	**ft_loadmap(char *path)
 	char	*added;
 	int		ffs;
 
-	ffs = open(path, O_RDONLY);
-	if (!ffs)
-		return (NULL);
-	res = NULL;
-	added = get_next_line(path);
-	while (added)
+	if (path)
 	{
-		res = ft_arrayjoin(res, added);
+		ffs = open(path, O_RDONLY);
+		if (ffs < 0)
+			ft_error_path();
+		res = NULL;
+		added = get_next_line(ffs);
+		while (added)
+		{
+			res = ft_arrayjoin(res, added);
+			added = ft_xfree(added);
+			added = get_next_line(ffs);
+		}
 		added = ft_xfree(added);
-		added = get_next_line(path);
+		close(ffs);
+		return (res);
 	}
-	close(ffs);
-	return (res);
+	return (NULL);
 }
