3d31844fadcebceb8cac19c8a1c9793ab5925008
Brian Rheaume-Michaud <brheaume@c2r5p9.42quebec.com>
2023-05-01 14:16:39 -0400

    modified ft_error_path into ft_error_simple

--- error.c
+++ error.c
@@ -6,22 +6,23 @@
 /*   By: brheaume <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 16:26:20 by brheaume          #+#    #+#             */
-/*   Updated: 2023/04/25 12:32:21 by brheaume         ###   ########.fr       */
+/*   Updated: 2023/05/01 10:19:31 by brheaume         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	ft_error_path(void)
+void	ft_error_simple(char *message)
 {
-	ft_putendl_fd("Invalid map path\n", ERROR_OUTPUT);
+	ft_putendl_fd("Error:", ERROR_OUTPUT);
+	ft_putendl_fd(message, ERROR_OUTPUT);
 	exit(EXIT_FAILURE);
 }
 
 void	ft_error_map(char **map)
 {
 	ft_clear_array(map);
-	ft_putendl_fd("Invalid map\n", ERROR_OUTPUT);
+	ft_putendl_fd("Error\nInvalid map content\n", ERROR_OUTPUT);
 	exit(EXIT_FAILURE);
 }
 
@@ -29,7 +30,7 @@ void	ft_error_game(t_data *game)
 {
 	ft_clear_array(game->map);
 	mlx_terminate(game->mlx);
-	ft_putendl_fd("Game did an oopsie daisy", ERROR_OUTPUT);
+	ft_putendl_fd("Error\nGame did an oopsie daisy", ERROR_OUTPUT);
 	exit(EXIT_FAILURE);
 }
 
